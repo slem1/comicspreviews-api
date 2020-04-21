f57b3f493a90e72aa8ba51d9a98c8309a5724df0
slem1 <sylvain.lem1@gmail.com>
2020-04-21 18:09:55 +0200

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,46 @@
 module Main where
 
 import Lib
+import Article
 
 import Web.Scotty
 
 import Data.Monoid (mconcat)
+import Network.HTTP.Types.Status
 
-main = scotty 3000 $
-  get "/:word" $ do
-    beam <- param "word"
-    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+main = scotty 3000 $ do
+  get "/" $ do     
+    text "This was a GET request!"     
+  delete "/" $ do
+    html "This was a DELETE request!"  
+  post "/" $ do
+    text "This was a POST request!"
+  put "/" $ do
+    text "This was a PUT request!"  
+  partJson
+  part2
+
+
+part2 :: ScottyM () 
+part2 = do 
+  post "/set-headers" $ do
+    status status302
+    setHeader "Location" "http://www.google.com"
+  get "/askfor/:word" $ do
+    w <- param "word"    
+    html $ mconcat ["<h1>You asked for ", w, ", you got it !</h1>"]
+  post "/submit" $ do
+    name <- param "name"
+    text name
+  matchAny "/all" $ do
+    text "matches all methods"
+  notFound $ do
+    text "there is no such route"
+
+partJson :: ScottyM ()
+partJson = do
+  get "/article" $ do
+    json $ Article 13 "caption" "content"
+  post "/article" $ do
+    article <- jsonData :: ActionM Article
+    json article
---
+++ src/Article.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Article (
+    Article(..)
+
+) where
+
+import Data.Text.Lazy
+import Data.Text.Lazy.Encoding
+import Data.Aeson
+import Control.Applicative
+
+data Article = Article Integer Text Text deriving (Show)
+
+instance FromJSON Article where
+    parseJSON (Object v) = Article <$>
+                            v .:? "id" .!= 0 <*>
+                            v .:  "title"    <*>
+                            v .:  "bodyText"  
+
+instance ToJSON Article where
+     toJSON (Article id title bodyText) =
+         object ["id" .= id,
+                 "title" .= title,
+                 "bodyText" .= bodyText]
